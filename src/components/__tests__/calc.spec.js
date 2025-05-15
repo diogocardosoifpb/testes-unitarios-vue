a32f603f2f65e25018ee8b4fdac9b3eb53c74103
Diogo Cardoso <diogomescardoso@gmail.com>
2025-05-15 11:01:00 -0300

    calculadora

---
+++ src/components/__tests__/calc.spec.js
@@ -0,0 +1,6 @@
+import { add, subtract, multiply, divide } from '../calc.js'
+import { describe, it, expect } from 'vitest'
+
+describe('Calculator', () => {
+  it('should add two numbers', () => {})
+})
