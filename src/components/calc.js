c49de4b65026210945632ddc6eecfe39e2dd8bba
Diogo Cardoso <148496505+diogocardosoifpb@users.noreply.github.com>
2025-08-09 08:21:39 -0300

    Update calc.js

--- src/components/calc.js
+++ src/components/calc.js
@@ -2,14 +2,11 @@ export function add(a, b) {
   return a + b
 }
 export function subtract(a, b) {
-  return a - b
+  //ToDo
 }
 export function multiply(a, b) {
-  return a * b
+  //ToDo
 }
 export function divide(a, b) {
-  if (b === 0) {
-    throw new Error('Cannot divide by zero')
-  }
-  return a / b
+  //ToDo
 }
